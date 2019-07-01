591b1e5a9b886278cbb61a823eb14bdc1e3f93e7
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-07-01 16:00:55 -0400

    map through book mark, can console log values

--- booksearch/src/form.js
+++ booksearch/src/form.js
@@ -1,47 +1,56 @@
-import React from 'react';
+import React from "react";
 
 class Form extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      searchTerm: ''
-    }
+      searchTerm: ""
+    };
   }
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault();
-    const myKey= 'AIzaSyC7etpGfup0-A3HssAIzYe_mlljnOo4iPE';
+    const myKey = "AIzaSyC7etpGfup0-A3HssAIzYe_mlljnOo4iPE";
     const searchTerm = this.state.searchTerm;
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${myKey}`
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${myKey}`;
     console.log(searchTerm);
     fetch(url)
-      .then(response => {response.ok ? response.json() : Promise.reject(response.statusText)})
+      .then(response =>
+        response.ok ? response.json() : Promise.reject(response.statusText)
+      )
       .then(data => {
-        console.log(Object.keys(data));
-        this.setState = {
+        data.items.map(book => {
+        book = {
+            title: book.volumeInfo.title,
+            authors: book.volumeInfo.authors,
+            retailPrice: book.saleInfo.retailPrice ?  book.saleInfo.retailPrice.amount : 'Price unavailable',
+            description: book.volumeInfo.description,
+            image: book.volumeInfo.imageLinks.thumbnail
+          };
+          console.log(book);
+        });
+        
+        this.setState = {};
+      });
+  };
 
-        }
-      })
-  }  
-
-  searchTermUpdated = (searchTerm) => {
-    console.log("State:", this.state);
-    console.log("Search term:", searchTerm);
+  searchTermUpdated = searchTerm => {
     this.setState({
       searchTerm
-    }) 
-     
-        
+    });
+  };
 
-  }
-
-  render(){
+  render() {
     return (
       <div className="form">
-        <form onSubmit= {(event)=> this.handleSubmit(event) }>
+        <form onSubmit={event => this.handleSubmit(event)}>
           <label htmlFor="search">Search</label>
-          <input name="search"type="text" onChange={event => this.searchTermUpdated(event.target.value)} />
+          <input
+            name="search"
+            type="text"
+            onChange={event => this.searchTermUpdated(event.target.value)}
+          />
           <label name="">Book Type</label>
-          <button type="submit"  >Search</button>
+          <button type="submit">Search</button>
           <select>
             <option value="partial">Partial</option>
             <option value="full">Full</option>
@@ -57,7 +66,7 @@ class Form extends React.Component {
           </select>
         </form>
       </div>
-    )
+    );
   }
 }
 
